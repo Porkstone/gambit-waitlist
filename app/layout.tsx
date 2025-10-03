89fb1e8df338679c7554ad00d66ce3850edc52e0
Charlie <charlie@hallville.net>
2025-10-03 17:57:53 +0100

    Updated title of page to Gambit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Gambit',
+  description: 'Your deal finding co-pilot',
+  generator: 'gambit.ai',
 }
 
 export default function RootLayout({
